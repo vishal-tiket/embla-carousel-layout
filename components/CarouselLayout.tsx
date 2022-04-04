3c7d2e81e73205fbfd67453e173ac241bf3c737c
vishal-tiket <vishal.kamra@tiket.com>
2022-04-04 17:06:08 +0530

    js into ts migration

--- components/CarouselLayout.tsx
+++ components/CarouselLayout.tsx
@@ -1,32 +1,66 @@
-import useEmblaCarousel from 'embla-carousel-react'
+import useEmblaCarousel, {
+  EmblaOptionsType,
+} from 'embla-carousel-react'
 import React, { useCallback, useEffect, useState } from "react"
 
+/** types (Props) for carousel layout
+  1. slidesToScroll => Group slides together. Drag interactions, dot navigation, and previous/next buttons are mapped to group slides into the given number.
+  2. dragFree => Enables momentum scrolling. The speed and duration of the continued scrolling is proportional to how vigorous the drag gesture is.
+  3. align => Align the slides relative to the carousel viewport. Use one of the predefined alignments start, center or end.
+  4. noOfItems => Number of items on carousel viewport.
+  5. items => Array of carousel items.
+  6. showProgressBar => To show/hide the progress bar.
+  7. showSlideHandlers =>  To show/hide the prev/next buttons.
+  8. containerId => Carousel container id required to calculate the width of child according to the carousel view port.
+  9. showIndicators => To show/hide the indicators of selected and non selected index/item
+*/
+
+type CarouselLayoutTypes = {
+  slidesToScroll:number,
+  dragFree: boolean,
+  align: "start" | "center" | "end",
+  noOfItems: number,
+  items: React.ReactNode[],     
+  showProgressBar: boolean,
+  showSlideHandlers: boolean,
+  containerId: string,
+  showIndicators: boolean 
+}
+
+// carousel layout hoc
 const CarouselLayout = ({
-    slidesToScroll=1, // slides to scroll from handlers 
-    showProgressBar=false, // to show/hide progresss bar
-    showSlideHandlers=true, // to show/hide handlers (prev/next btn)
-    children, // card items
-    containerId="someId", // conatiner id (used in the calculation of card items width),
-    showIndicators=true // to show/hide indicators
-  }) => {
+  slidesToScroll=1, // defalt values
+  noOfItems=1,
+  align="center",
+  dragFree=false,
+  showProgressBar=false,
+  showSlideHandlers=false,
+  items,
+  containerId="carousel_id",
+  showIndicators=false
+}: CarouselLayoutTypes) => {
     
+    // options of useEmblaCarousel hook
+    const options: EmblaOptionsType = {
+      slidesToScroll,
+      align,
+      dragFree
+    }
+
     // embla carousel hook
-    const [emblaRef, embla] = useEmblaCarousel({
-        slidesToScroll,
-        skipSnaps: false,
-        align: "center"
-    });
+    const [emblaRef, embla] = useEmblaCarousel(options);
 
     // states to handle previous, next button, selected index, scrollbar progress etc
-    const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
-    const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
-    const [scrollProgress, setScrollProgress] = useState(0)
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const [scrollSnaps, setScrollSnaps] = useState([]);
+    const [prevBtnEnabled, setPrevBtnEnabled] = useState<boolean>(false);
+    const [nextBtnEnabled, setNextBtnEnabled] = useState<boolean>(false);
+    const [scrollProgress, setScrollProgress] = useState<number>(0)
+    const [selectedIndex, setSelectedIndex] = useState<number>(0);
+    const [scrollSnaps, setScrollSnaps] = useState<number[] | []>([]);
+
+    // scroll to a particular slide index
     const scrollTo = useCallback((index) => embla && embla.scrollTo(index), [
       embla
     ]);
-
 
     // previous button handler
     const scrollPrev = useCallback(() => embla && embla.scrollPrev(), [embla]);
@@ -37,54 +71,59 @@ const CarouselLayout = ({
     // on scroll handler to calculate scrollbar progress
     const onScroll = useCallback(() => {
       if (!embla) return;
+
       const progress = Math.max(0, Math.min(1, embla.scrollProgress()));
       setScrollProgress(progress * 100);
+
     }, [embla, setScrollProgress]);
 
     // handle when to disable previous and next button
     const onSelect = useCallback(() => {
       if (!embla) return;
+
       setSelectedIndex(embla.selectedScrollSnap());
       setPrevBtnEnabled(embla.canScrollPrev());
       setNextBtnEnabled(embla.canScrollNext());
+
     }, [embla, setSelectedIndex]);
 
     // event handlers for embla carousel
     useEffect(() => {
       if (!embla) return;
+
       onSelect()
       onScroll();
       embla.on("select", onSelect);
       embla.on("scroll", onScroll);
+      setScrollSnaps(embla.scrollSnapList());
 
-    setScrollSnaps(embla.scrollSnapList());
     }, [embla, onScroll, onSelect, setScrollSnaps]);
 
     // calculating width of slides in carousel
     useEffect(()=>{
-        const slidesArr = document.querySelector(`#${containerId}`)?.childNodes
-        slidesArr?.forEach((el, index)=>{
-            slidesArr[index].style.flex = `0 0 calc(${(100/slidesToScroll)}% - 10px)`;
-        })
+      const slidesArr: NodeListOf<ChildNode> = document.querySelector(`#${containerId}`)?.childNodes
+      slidesArr?.forEach((el: HTMLElement)=>{
+          el.style.flex = `0 0 calc(${(100/noOfItems)}% - 10px)`;
+      })
     }, [])
 
-    // indicators
+    // item indicators
     const renderIndicators = showIndicators && scrollSnaps.map((_, index) => (
       <span
         key={index}
         className={index === selectedIndex ? "active" : ""}
         onClick={() => scrollTo(index)}
       >
-        </span>
+      </span>
     )) || null
 
     return (
         <>
 
-          {/* children ( card items ) */}
+          {/* items ( card items ) */}
           <div className="embla3" ref={emblaRef}>
             <div className="embla__container3" id={containerId}>
-              {children}
+              {items}
             </div>
           </div>
 
@@ -105,11 +144,11 @@ const CarouselLayout = ({
             </>}
           </div>
 
-          {/* indicators */}
+          {/* item indicators */}
           <div className='indicators'>
             {renderIndicators}
           </div>
-
+          
         </>
     )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import CarouselLayout from "../components/CarouselLayout"
 
-const Home = () => {
+const Home = (): JSX.Element => {
 
   // card items
-  const CardItems = [1,2,3,4,5,6].map((el, index)=>{
+  const CardItems: JSX.Element[] = [1,2,3,4,5,6].map((el, index)=>{
     return (
       <div key={index} className="card">
         {el}
@@ -20,57 +20,72 @@ const Home = () => {
 
           <CarouselLayout
             slidesToScroll={1}
+            noOfItems={1}
+            dragFree={false}
+            align="center"
             showProgressBar={true}
             showSlideHandlers={true}
             containerId="someId"
-          >
-            {CardItems}
-          </CarouselLayout>
+            showIndicators={true}
+            items={CardItems}
+          />
 
           <h2>2 item on 1 slide</h2>
 
           <CarouselLayout
             slidesToScroll={2}
+            noOfItems={2}
+            dragFree={false}
+            align="center"
             showProgressBar={true}
             showSlideHandlers={true}
             containerId="someId1"
-          >
-            {CardItems}
-          </CarouselLayout>
+            showIndicators={true}
+            items={CardItems}
+          />
 
           <h2>3 item on 1 slide</h2>
 
           <CarouselLayout
             slidesToScroll={3}
+            noOfItems={3}
+            dragFree={false}
+            align="center"
             showProgressBar={true}
             showSlideHandlers={true}
             containerId="someId2"
-          >
-            {CardItems}
-          </CarouselLayout>
+            showIndicators={true}
+            items={CardItems}
+          />
 
 
           <h2>4 item on 1 slide</h2>
 
           <CarouselLayout
             slidesToScroll={4}
+            noOfItems={4}
+            dragFree={false}
+            align="center"
             showProgressBar={true}
             showSlideHandlers={true}
             containerId="someId3"
-          >
-            {CardItems}
-          </CarouselLayout>
+            showIndicators={true}
+            items={CardItems}
+          />
 
           <h2>2.4 (size) item on 1 slide (for first carousel on homepage) and can be used as 3.4, 4.4 depending on requirement</h2>
 
           <CarouselLayout
             slidesToScroll={2.4}
+            noOfItems={2.4}
+            dragFree={false}
+            align="start"
             showProgressBar={true}
             showSlideHandlers={true}
             containerId="someId4"
-          >
-            {CardItems}
-          </CarouselLayout>
+            showIndicators={true}
+            items={CardItems}
+          />
 
       </div>
   )
